acf23779478758174a781db9c3eb99b7e1c99c0f
zhouzhuo <syhgzz@163.com>
2025-02-11 12:46:01 +0800

    允许所有ip访问. 调用LineGenerator v0.1

--- wenwu_server/settings.py
+++ wenwu_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*3o#2qqffxk7gxgbpk&#p*zxsddd7&ih9mhecc^uxk&fj1(si4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.98.104']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
