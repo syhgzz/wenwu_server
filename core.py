d158498c70f1d43e3229b4a897af2267a2ec4da4
zhouzhuo <syhgzz@163.com>
2025-02-11 10:30:58 +0800

    6

--- core.py
+++ core.py
@@ -11,7 +11,7 @@ def img_process(input_file_path,output_file_path):
     print("处理图像中")
     # shutil.copy(input_file_path,output_file_path)
 
-    lg = ctypes.CDLL("/usr/lib/libLineGenerator.so")
+    lg = ctypes.CDLL("./libLineGenerator.so")
 
     # 定义函数参数和返回类型
     lg.go_py.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
